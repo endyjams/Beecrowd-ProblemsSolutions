ffb277008b813a734070eb75f8e8a84f91168d39
endyjams <endy.jams@gmail.com>
2022-09-27 15:20:39 -0300

    1234.cpp beecrowd

---
+++ 1234.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; //-9 * 10^18 to 9 * 10^18
+typedef unsigned long long int ull; //very big positive integer
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < b; i++)
+#define w(t) while(t--)
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    char s[51];
+
+    while (cin.getline(s, 51))
+    {
+        int x = 0;
+
+        for (int i = 0; i < 51 && s[i] != '\0'; i++)
+        {
+            if (isalpha(s[i])) x++;
+
+            if (x % 2) s[i] = toupper(s[i]);
+            else s[i] = tolower(s[i]);     
+        }
+
+        for (int i = 0; i < 51 && s[i] != '\0'; i++) cout << s[i];
+        
+        cout << '\n';
+    }
+
+    return 0;
+}
