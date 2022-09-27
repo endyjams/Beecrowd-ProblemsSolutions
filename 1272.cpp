3aa434331b91521d79eec52e8fe22053a723702e
endyjams <endy.jams@gmail.com>
2022-09-27 18:23:22 -0300

    1272

---
+++ 1272.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll; //-9 * 10^18 to 9 * 10^18
+typedef unsigned long long int ull; //very big positive integer
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < b; i++)
+#define w(t) while(t--)
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin >> t;
+    char s[100];
+    cin.getline(s, 100);
+    
+    w(t)
+    {
+        cin.getline(s, 100);
+        
+        bool w = true;
+        
+        for (int i = 0; i < 100 && s[i] != '\0'; i++)
+        {
+            if (isalpha(s[i]) && w)
+            {
+                cout << s[i];
+                w = false;
+            }
+            else if (!isalpha(s[i]))
+            {
+                w = true;
+            }
+        }
+        
+        cout << '\n';
+    }
+
+    return 0;
+}
